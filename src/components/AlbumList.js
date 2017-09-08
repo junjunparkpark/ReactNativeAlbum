2181566dbcafc2b5bca1e80b7185020dbe764d61
Jun Park <jun.park@live.com>
2017-09-08 16:57:07 -0700

    Change AlbumList from Functional component to a Class component. Add Ajax Axios request method.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
+class AlbumList extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <View>
-      <Text>Album List</Text>
-    </View>
-  );
+    this.state = {
+      albums: []
+    };
+  }
+
+  fetchAlbums() {
+    axios.get({
+
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List</Text>
+      </View>
+    );
+  }
 };
 
 export default AlbumList;
